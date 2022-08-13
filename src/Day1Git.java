58ae45c7f0aa12bb882ffde80397615e1a82e5a9
ibrahim <bcelikel1994@gmail.com>
2022-08-13 22:02:49 +0300

    3.C

--- src/Day1Git.java
+++ src/Day1Git.java
@@ -1,6 +1,7 @@
 public class Day1Git {
     public static void main(String[] args) {
         System.out.println("git 1. deneme");
-        System.out.println("git 1. deneme");
+        System.out.println("git 2. deneme");
+        System.out.println("git 3. deneme");
     }
 }
