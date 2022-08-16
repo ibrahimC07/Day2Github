602950b6609c44c5a8aec391a0668bb8445ec33a
ibrahim <bcelikel1994@gmail.com>
2022-08-16 22:54:45 +0300

    12

---
+++ src/Gitignore.java
@@ -0,0 +1,7 @@
+public class Gitignore {
+    public static void main(String[] args) {
+        System.out.println("gitignore deneme 1");
+        System.out.println("gitignore deneme 2");
+
+    }
+}
